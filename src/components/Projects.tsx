94dc3b2be854b66cee213f2a7dcab423f7132dac
Ahmedezz570 <ahmedez570@gmail.com>
2025-06-01 01:42:28 +0300

    add local route for test deployment

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,24 +1,24 @@
 
 import ProjectCard from "./ProjectCard";
-
+import React , {useState , useEffect} from "react";
 const Projects = () => {
-  const projects = [
-    {
-      title: "Striker",
-      description: "A fully responsive e-commerce website with product filtering, cart functionality, and secure checkout process. Built with React and integrated with a headless CMS for content management.",
-      image: "./Striker.png",
-      tags: ["React", "Node.js", "MongoDB", "CSS"],
-      liveUrl: "https://project-cs-309.vercel.app/",
-      githubUrl: "https://github.com/Ahmedezz570/Striker-",
-    },
-    {
-      title: "SST-LabRent",
-      description: " SST is a full-stack web application designed to manage and facilitate the rental process of laboratory tools for the Air Department - Faculty of Engineering.",
-      image: "SST.png",
-      tags: ["React", "Node.js", "MongoDB", "tailwind"],
-      liveUrl: "notFound",
-      githubUrl: "https://github.com/Ahmedezz570/SST-Rent",
-    },
+  // const projects = [
+  //   {
+  //     title: "Striker",
+  //     description: "A fully responsive e-commerce website with product filtering, cart functionality, and secure checkout process. Built with React and integrated with a headless CMS for content management.",
+  //     image: "./Striker.png",
+  //     tags: ["React", "Node.js", "MongoDB", "CSS"],
+  //     liveUrl: "https://project-cs-309.vercel.app/",
+  //     githubUrl: "https://github.com/Ahmedezz570/Striker-",
+  //   },
+  //   {
+  //     title: "SST-LabRent",
+  //     description: " SST is a full-stack web application designed to manage and facilitate the rental process of laboratory tools for the Air Department - Faculty of Engineering.",
+  //     image: "SST.png",
+  //     tags: ["React", "Node.js", "MongoDB", "tailwind"],
+  //     liveUrl: "notFound",
+  //     githubUrl: "https://github.com/Ahmedezz570/SST-Rent",
+  //   },
     // {
     //   title: "Task Management App",
     //   description: "A full-stack task management application with user authentication, task creation, editing, and categorization features. Includes drag-and-drop functionality and data visualization.",
@@ -27,8 +27,22 @@ const Projects = () => {
     //   liveUrl: "https://example.com",
     //   githubUrl: "https://github.com",
     // },
-  ];
-
+  //];
+const [projects , setprojects] = useState([]);
+useEffect(() => {
+  const fetchProjects = async () => {
+    try {
+      const response = await fetch("http://localhost:7000/projects");
+      const data = await response.json();
+      // const filteredProjects = data.filter(repo => repo.topics.includes("project"));
+      setprojects(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+    }
+  }
+  fetchProjects();
+}, []);
   return (
     <section id="projects" className="py-20 bg-ahmed-muted">
       <div className="container px-4 mx-auto">
